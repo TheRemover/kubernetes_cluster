8c823291b55e172010879a9d81c2aa9e132d6bd7
TheRemover <theremover@moonslanding.nt>
2020-05-07 23:49:01 +0000

    seperated conditions to specify a list of node names

--- cluster.py
+++ cluster.py
@@ -2,8 +2,9 @@ from cluster_power.cluster_power import Cluster
 
 def main():
   cluster = Cluster()
+  print(cluster.nodes)
   cluster.power_off()
-#  cluster.power_on(name="TK1C")
+  cluster.power_off(name="N1R")
 #  cluster.power_on(name="N1L")
 #  cluster.power_on(name="N3R")
 #  cluster.get_state()
--- cluster_power/cluster_power.py
+++ cluster_power/cluster_power.py
@@ -1,5 +1,6 @@
 import serial
 import json
+import time
 
 class Cluster:
 
@@ -12,17 +13,35 @@ class Cluster:
 
   def power_off(self, type=None, name=None):
     for node in self.nodes:
+      print(node)
       if type is None and name is None:
         self.ser.write("{} {}".format(node['relay'], 1).encode())
-      elif (node['type']==type or node['node'] in name):
-        self.ser.write("{} {}".format(node['relay'], 1).encode())
+      elif type is not None:
+        if node['type']==type:
+          print("{} {}".format(node['node'],node['relay']))
+          time.sleep(10)
+          self.ser.write("{} {}".format(node['relay'], 1).encode())
+      elif name is not None:
+        if node['node'] in name:
+          print("{} {}".format(node['node'],node['relay']))
+          time.sleep(10)
+          self.ser.write("{} {}".format(node['relay'], 1).encode())
 
   def power_on(self,type=None, name=None):
+    print(name)
     for node in self.nodes:
       if type is None and name is None:
         self.ser.write("{} {}".format(node['relay'], 0).encode())
-      elif node['type']==type or node['node']==name:
-        self.ser.write("{} {}".format(node['relay'], 0).encode())
+      elif type is not None:
+        if node['type']==type:
+          print("{} {}".format(node['node'],node['relay']))
+          time.sleep(10)
+          self.ser.write("{} {}".format(node['relay'], 0).encode())
+      elif name is not None:
+        if node['node'] in name:
+          print("{} {}".format(node['node'],node['relay']))
+          time.sleep(10)
+          self.ser.write("{} {}".format(node['relay'], 0).encode())
 
   def get_state(self):
     for node in self.nodes:
