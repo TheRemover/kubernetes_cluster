8d2b9281221a2649d01e4c3c27042faf095c9934
TheRemover <theremover@moonslanding.nt>
2020-05-08 00:03:34 +0000

    Reduced sleep and removed extra print statements

--- cluster_power/cluster_power.py
+++ cluster_power/cluster_power.py
@@ -13,39 +13,31 @@ class Cluster:
 
   def power_off(self, type=None, name=None):
     for node in self.nodes:
-      print(node)
+      time.sleep(1)
       if type is None and name is None:
         self.ser.write("{} {}".format(node['relay'], 1).encode())
       elif type is not None:
         if node['type']==type:
-          print("{} {}".format(node['node'],node['relay']))
-          time.sleep(10)
           self.ser.write("{} {}".format(node['relay'], 1).encode())
       elif name is not None:
         if node['node'] in name:
-          print("{} {}".format(node['node'],node['relay']))
-          time.sleep(10)
           self.ser.write("{} {}".format(node['relay'], 1).encode())
 
   def power_on(self,type=None, name=None):
-    print(name)
     for node in self.nodes:
+      time.sleep(1)
       if type is None and name is None:
         self.ser.write("{} {}".format(node['relay'], 0).encode())
       elif type is not None:
         if node['type']==type:
-          print("{} {}".format(node['node'],node['relay']))
-          time.sleep(10)
           self.ser.write("{} {}".format(node['relay'], 0).encode())
       elif name is not None:
         if node['node'] in name:
-          print("{} {}".format(node['node'],node['relay']))
-          time.sleep(10)
           self.ser.write("{} {}".format(node['relay'], 0).encode())
 
-  def get_state(self):
-    for node in self.nodes:
-      if GPIO.input(node['gpio']):
-        print('{} - ON'.format(node['node']))
-      else:
-        print('{} - OFF'.format(node['node']))
+  #def get_state(self):
+    #for node in self.nodes:
+      #if GPIO.input(node['gpio']):
+        #print('{} - ON'.format(node['node']))
+      #else:
+        #print('{} - OFF'.format(node['node']))
