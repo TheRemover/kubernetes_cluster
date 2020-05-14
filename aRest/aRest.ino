4829cc3593249dd62decafcd3ee794dc6a6c859b
TheRemover <theremover@moonslanding.nt>
2020-05-14 19:16:29 +0000

    added REST API for relay control

---
+++ aRest/aRest.ino
@@ -0,0 +1,65 @@
+// Libraries
+#include <SPI.h>
+#include <Ethernet.h>
+#include <aREST.h>
+#include <avr/wdt.h>
+
+// Enter a MAC address for your controller below.
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xFE, 0x40 };
+
+// IP address in case DHCP fails
+IPAddress ip(172,16,0,2);
+
+// Ethernet server
+EthernetServer server(80);
+
+// Create aREST instance
+aREST rest = aREST();
+
+// Declare functions to be exposed to the API
+int initialize();
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(9600);
+  Serial.println("Initializing relays");
+  initialize();
+
+  // Give name & ID to the device (ID should be 6 characters long)
+  rest.set_id("000001");
+  rest.set_name("power_relay");
+
+  // Start the Ethernet connection and the server
+  Ethernet.begin(mac, ip);
+  
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  // Start watchdog
+  wdt_enable(WDTO_4S);
+}
+
+void loop() {
+
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  rest.handle(client);
+  wdt_reset();
+
+}
+
+int initialize() {
+
+  for (int i = 22; i <=29; i++) {
+    pinMode(i,OUTPUT);
+    digitalWrite(i,HIGH);
+  }
+
+  for (int i = 39; i <=46; i++) {
+    pinMode(i,OUTPUT);
+    digitalWrite(i,HIGH);
+  }
+
+}
